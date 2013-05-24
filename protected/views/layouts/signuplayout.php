786ad832ba1ed3cd8405c23d8a3dc9fff5bfd63d
rsnanda <rsnanda@RSNANDA-IN.in.oracle.com>
2013-05-24 19:49:46 +0530

    changed the layout for signup page and modified form.php to use the block-layout classes

--- protected/views/layouts/signuplayout.php
+++ protected/views/layouts/signuplayout.php
@@ -5,22 +5,61 @@ and open the template in the editor.
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Updates</title>
+        <title>Sign Up!</title>
         <script src="http://code.jquery.com/jquery.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <link href="css/bootstrap.css" rel="stylesheet">
         <link href="css/bootstrap-responsive.css" rel="stylesheet">
-        <link href="css/wallposts.css" rel="stylesheet">
         <link href="css/site.css" rel="stylesheet">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body style="background-color:white ">
-        <div class="headermain">
+        <div class="header">
             <h1  class="pull-left"> Connect! </h1> 
-            <a class="pull-right" href="signout" ><i class="icon-off"></i></a>
         </div>
         <div class="container-fluid">
-            <div class="wpcenter-container">  <?php echo $content; ?></div>
+            <div class="left-container"> 
+                <div class="row-fluid">
+                <div class="span6">
+                    <img class="img-polaroid" src="images/tulips.jpeg"/>
+                </div>
+                    <div class="span1"> </div>
+                <div class="span5">
+                    <p class="lead heading_em">Make Friends !</p>
+                        <p class="">Make new Friends We'll Give Suggestions!</p
+                </div>
+                    </div>
+            </div>
+            <hr class="feature-divider"/>
+            <div class="row-fluid">
+              
+                <div class="span5">
+                    <p class="lead heading_em">Make Friends !</p>
+                        <p class="">Make new Friends We'll Give Suggestions!</p>
+                </div>
+                <div class="span1"></div>
+                  <div class="span6">
+                    <img class="img-polaroid" src="images/water.jpeg"/>
+                </div>
+                    </div>
+             <hr class="feature-divider"/>
+             <div class="row-fluid">
+                <div class="span6">
+                    <img class="img-polaroid" src="images/tulips.jpeg"/>
+                </div>
+                   <div class="span1"></div>
+                <div class="span5">
+                    <p class="lead heading_em">Make Friends !</p>
+                        <p class="">Make new Friends We'll Give Suggestions!</p
+                </div>
+                    </div>
+            </div>
+            
+        </div>
+            <div class="signup">
+                
+                 <?php echo $content; ?>
+            </div>
             <div class="sitefooter">
                 <ul class="inline  pull-right">
                     <li>
--- protected/views/userLoginModel/form.php
+++ protected/views/userLoginModel/form.php
@@ -3,44 +3,43 @@
 /* @var $model UserLoginModel */
 /* @var $form CActiveForm */
 ?>
-<div class="wide form">
+
 
     <?php
     $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
         'id' => 'user-login-model-form',
-        'type' => 'horizontal',
+          'htmlOptions' => array('class' => 'form-signin'),
         'enableClientValidation' => true,
         'clientOptions' => array(
             'validateOnSubmit' => true,
-            'validateOnChange' => true,
-        )
+            'validateOnChange' => true,)
     ));
     ?>
-
+    <h2 class="form-signin-heading">Sign Up !</h2>
     <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-    <?php echo $form->dropdownListRow($model, 'TITLE', array('Select' => 'Select', 'Mr' => 'Mr', "Miss" => "Miss", 'Dr' => 'Dr')); ?>
+    <?php echo $form->dropdownListRow($model, 'TITLE', array('Select' => 'Select', 'Mr' => 'Mr', "Miss" => "Miss", 'Dr' => 'Dr'),array('class' => 'input-block-level')); ?>
 
-    <?php echo $form->textFieldRow($model, 'USER_NAME', array('size' => 60, 'maxlength' => 200)); ?>
+    <?php echo $form->textFieldRow($model, 'USER_NAME', array('size' => 60, 'class' => 'input-block-level','maxlength' => 200)); ?>
 
-    <?php echo $form->passwordFieldRow($model, 'PASSWORD', array('size' => 60, 'maxlength' => 200)); ?>
+    <?php echo $form->passwordFieldRow($model, 'PASSWORD', array('size' => 60, 'class' => 'input-block-level','maxlength' => 200)); ?>
 
-    <?php echo $form->passwordFieldRow($model, 'repeat_password', array('size' => 60, 'maxlength' => 200)); ?>
+    <?php echo $form->passwordFieldRow($model, 'repeat_password', array('size' => 60,'class' => 'input-block-level', 'maxlength' => 200)); ?>
 
-    <?php echo $form->textFieldRow($model, 'FIRST_NAME', array('size' => 45, 'maxlength' => 45)); ?>
+    <?php echo $form->textFieldRow($model, 'FIRST_NAME', array('size' => 45, 'class' => 'input-block-level','maxlength' => 45)); ?>
 
-    <?php echo $form->textFieldRow($model, 'MIDDLE_NAME', array('size' => 45, 'maxlength' => 45)); ?>
+    <?php echo $form->textFieldRow($model, 'MIDDLE_NAME', array('size' => 45, 'class' => 'input-block-level','maxlength' => 45)); ?>
 
-    <?php echo $form->textFieldRow($model, 'LAST_NAME', array('size' => 60, 'maxlength' => 200)); ?>
+    <?php echo $form->textFieldRow($model, 'LAST_NAME', array('size' => 60, 'class' => 'input-block-level','maxlength' => 200)); ?>
 
-    <?php echo $form->textFieldRow($model, 'PHONE_NUMBER'); ?>
+    <?php echo $form->textFieldRow($model, 'PHONE_NUMBER',array('class' => 'input-block-level')); ?>
 
-    <?php echo $form->textFieldRow($model, 'MOBILE_NUMBER'); ?>
+    <?php echo $form->textFieldRow($model, 'MOBILE_NUMBER',array('class' => 'input-block-level')); ?>
 
-    <?php echo $form->dropdownListRow($model, 'GENDER', array('Select' => 'Select', 0 => 'Male', 1 => "Female",)); ?>
+    <?php echo $form->dropdownListRow($model, 'GENDER', array('Select' => 'Select',0 => 'Male', 1 => "Female",),array('class' => 'input-block-level')); ?>
 
-    <?php echo $form->textFieldRow($model, 'EMAIL_ID', array('size' => 45, 'maxlength' => 45)); ?>
-    <?php echo $form->textFieldRow($model, 'DOB'); ?>
+    <?php echo $form->textFieldRow($model, 'EMAIL_ID', array('size' => 45, 'class' => 'input-block-level','maxlength' => 45)); ?>
+    <?php echo $form->textFieldRow($model, 'DOB',array('class' => 'input-block-level')); ?>
 
 <!--    <label for="Event_event_date" class="required">DOB <span class="required">*</span>
         <?php
@@ -57,17 +56,22 @@
         ));
         ?>
     </label>-->
+    <div class="inline">
     <?php
     $this->widget('bootstrap.widgets.TbButton', array(
         'buttonType' => 'submit',
         'type' => 'primary',
         'label' => 'Save',
-    ));
+        'htmlOptions' => array('class'=>'btn-bar')
+    )); ?>
+        
+        <?php
     $this->widget('bootstrap.widgets.TbButton', array(
         'buttonType' => 'reset',
         'label' => 'Reset',
+        'htmlOptions' => array('class'=>'btn-bar')
     ));
     ?>
+        </div>
     <?php $this->endWidget(); ?>
-
-</div><!-- form -->
+<!-- form -->
