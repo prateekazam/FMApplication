fc948d696be840427cc26db51a2c8db532b07d65
prateek <>
2013-05-25 19:04:42 +0530

    modified value of relationship

--- protected/models/RelationMaster.php
+++ protected/models/RelationMaster.php
@@ -6,7 +6,7 @@
  * The followings are the available columns in table 'fm_relation_mst':
  * @property integer $FAMILY_REL_ID
  * @property string $FAMILY_REL_DESC
- * @property string $FAMILY_REL_MST
+ * @property string $FAMILY_REL_NAME
  * @property string $CREATED_BY
  * @property string $CREATED_DATE
  */
@@ -35,14 +35,14 @@ class RelationMaster extends CActiveRecord {
         // NOTE: you should only define rules for those attributes that
         // will receive user inputs.
         return array(
-            array('FAMILY_REL_ID, FAMILY_REL_DESC, FAMILY_REL_MST, CREATED_BY', 'required'),
+            array('FAMILY_REL_ID, FAMILY_REL_DESC, FAMILY_REL_NAME, CREATED_BY', 'required'),
             array('FAMILY_REL_ID', 'numerical', 'integerOnly' => true),
-            array('FAMILY_REL_DESC, FAMILY_REL_MST', 'length', 'max' => 200),
+            array('FAMILY_REL_DESC, FAMILY_REL_NAME', 'length', 'max' => 200),
             array('CREATED_BY', 'length', 'max' => 100),
             array('CREATED_DATE', 'safe'),
             // The following rule is used by search().
             // Please remove those attributes that should not be searched.
-            array('FAMILY_REL_ID, FAMILY_REL_DESC, FAMILY_REL_MST, CREATED_BY, CREATED_DATE', 'safe', 'on' => 'search'),
+            array('FAMILY_REL_ID, FAMILY_REL_DESC, FAMILY_REL_NAME, CREATED_BY, CREATED_DATE', 'safe', 'on' => 'search'),
         );
     }
 
@@ -63,7 +63,7 @@ class RelationMaster extends CActiveRecord {
         return array(
             'FAMILY_REL_ID' => 'Family Rel',
             'FAMILY_REL_DESC' => 'Family Rel Desc',
-            'FAMILY_REL_MST' => 'Family Rel Mst',
+            'FAMILY_REL_NAME' => 'Family Rel Name',
             'CREATED_BY' => 'Created By',
             'CREATED_DATE' => 'Created Date',
         );
@@ -81,7 +81,7 @@ class RelationMaster extends CActiveRecord {
 
         $criteria->compare('FAMILY_REL_ID', $this->FAMILY_REL_ID);
         $criteria->compare('FAMILY_REL_DESC', $this->FAMILY_REL_DESC, true);
-        $criteria->compare('FAMILY_REL_MST', $this->FAMILY_REL_MST, true);
+        $criteria->compare('FAMILY_REL_NAME', $this->FAMILY_REL_NAME, true);
         $criteria->compare('CREATED_BY', $this->CREATED_BY, true);
         $criteria->compare('CREATED_DATE', $this->CREATED_DATE, true);
 
