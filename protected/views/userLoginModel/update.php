a7403824c6b59bb4b142b6d10b86e64cc086d70a
prateek <>
2013-05-26 14:31:11 +0530

    changed update code

--- protected/views/userLoginModel/update.php
+++ protected/views/userLoginModel/update.php
@@ -1,21 +1,43 @@
 <?php
-/* @var $this UserLoginModelController */
-/* @var $model UserLoginModel */
-
-$this->breadcrumbs=array(
-	'User Login Models'=>array('index'),
-	$model->TITLE=>array('view','id'=>$model->USER_ID),
-	'Update',
-);
-
-$this->menu=array(
-	array('label'=>'List UserLoginModel', 'url'=>array('index')),
-	array('label'=>'Create UserLoginModel', 'url'=>array('create')),
-	array('label'=>'View UserLoginModel', 'url'=>array('view', 'id'=>$model->USER_ID)),
-	array('label'=>'Manage UserLoginModel', 'url'=>array('admin')),
-);
+$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+    'id' => 'user-login-model-form1',
+    'enableAjaxValidation' => true,
+    'htmlOptions' => array(),
+    'enableClientValidation' => true,
+    'clientOptions' => array(
+        'validateOnSubmit' => true,
+        'validateOnChange' => true,
+        'enableAjaxValidation' => true,
+    )
+        ));
 ?>
+<?php echo $form->errorSummary($model); ?>
+<?php echo $form->dropdownListRow($model, 'TITLE', array('Select' => 'Select', 'Mr' => 'Mr', "Miss" => "Miss", 'Dr' => 'Dr'), array('class' => 'input-block-level')); ?>
+<?php echo $form->textFieldRow($model, 'FIRST_NAME', array('size' => 45, 'class' => 'input-block-level', 'maxlength' => 45)); ?>
 
-<h1>Update UserLoginModel <?php echo $model->USER_ID; ?></h1>
+<?php echo $form->textFieldRow($model, 'MIDDLE_NAME', array('size' => 45, 'class' => 'input-block-level', 'maxlength' => 45)); ?>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php echo $form->textFieldRow($model, 'LAST_NAME', array('size' => 60, 'class' => 'input-block-level', 'maxlength' => 200)); ?>
+
+<?php echo $form->textFieldRow($model, 'PHONE_NUMBER', array('class' => 'input-block-level')); ?>
+
+<?php echo $form->textFieldRow($model, 'MOBILE_NUMBER', array('class' => 'input-block-level')); ?>
+
+<?php echo $form->error($model, 'DOB'); ?>
+<div class="inline center-wrapper">
+    <?php
+    $this->widget('bootstrap.widgets.TbButton', array(
+        'buttonType' => 'submit',
+        'type' => 'primary',
+        'label' => 'Save',
+    ));
+    ?>
+    <div class="horizontalspacersmall"></div>
+    <?php
+    $this->widget('bootstrap.widgets.TbButton', array(
+        'buttonType' => 'reset',
+        'label' => 'Reset',
+    ));
+    ?>
+</div>
+<?php $this->endWidget(); ?>
