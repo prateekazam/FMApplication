715b76a2f2c665c8e5dcddef6b00c3b8a417c1ba
prateek <>
2013-05-26 14:30:25 +0530

    added emaill validation

--- protected/controllers/FamilyRegistrationController.php
+++ protected/controllers/FamilyRegistrationController.php
@@ -26,11 +26,11 @@ class FamilyRegistrationController extends Controller {
     public function accessRules() {
         return array(
             array('allow', // allow all users to perform 'index' and 'view' actions
-                'actions' => array('index', 'view','create'),
+                'actions' => array('index', 'view'),
                 'users' => array('*'),
             ),
             array('allow', // allow authenticated user to perform 'create' and 'update' actions
-                'actions' => array('update'),
+                'actions' => array('update','create'),
                 'users' => array('@'),
             ),
             array('allow', // allow admin user to perform 'admin' and 'delete' actions
