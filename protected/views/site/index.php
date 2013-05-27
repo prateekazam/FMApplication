20048d1fa7ec3a1caa2526516daffbf86994638b
prateek <>
2013-05-27 12:59:02 +0530

    added TbAlert message which will come after updating user information

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,12 +1,24 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name;
+$this->pageTitle = Yii::app()->name;
+?>
+<?php
+$this->widget('bootstrap.widgets.TbAlert', array(
+    'block' => true, // display a larger alert block?
+    'fade' => true, // use transitions?
+    'closeText' => '&times;', // close link text - if set to false, no close link is displayed
+    'alerts' => array(// configurations per alert type
+        'success' => array('block' => true, 'fade' => true, 'closeText' => '&times;'), // success, info, warning, error or danger
+    ),
+));
 ?>
 
-<?php $this->beginWidget('bootstrap.widgets.TbHeroUnit',array(
-    'heading'=>'Welcome to '.CHtml::encode(Yii::app()->name),
-)); ?>
+<?php
+$this->beginWidget('bootstrap.widgets.TbHeroUnit', array(
+    'heading' => 'Welcome to ' . CHtml::encode(Yii::app()->name),
+));
+?>
 
 
 
