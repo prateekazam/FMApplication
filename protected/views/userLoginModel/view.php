87add2af24f70f85d5419cc38ab3cb9ceff5e3f8
prateek <>
2013-05-30 20:44:15 +0530

    showing family records

--- protected/views/userLoginModel/view.php
+++ protected/views/userLoginModel/view.php
@@ -1,30 +1,35 @@
 <?php
-/* @var $this UserLoginModelController */
-/* @var $model UserLoginModel */
-
-$this->breadcrumbs=array(
-	'User Login Models'=>array('index'),
-	$model->TITLE,
-);
-
-$this->menu=array(
-	array('label'=>'List UserLoginModel', 'url'=>array('index')),
-	array('label'=>'Create UserLoginModel', 'url'=>array('create')),
-	array('label'=>'Update UserLoginModel', 'url'=>array('update', 'id'=>$model->USER_ID)),
-	array('label'=>'Delete UserLoginModel', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->USER_ID),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage UserLoginModel', 'url'=>array('admin')),
-);
+echo "<h4>User Information</h4>";
+$this->widget('zii.widgets.CDetailView', array(
+    'data' => $model,
+    'attributes' => array(
+        'TITLE',
+        'FIRST_NAME',
+        'MIDDLE_NAME',
+        'LAST_NAME',
+        'PHONE_NUMBER',
+        'MOBILE_NUMBER',
+        'EMAIL_ID',
+        'DOB',
+    ),
+));
 ?>
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'TITLE',
-		'FIRST_NAME',
-		'MIDDLE_NAME',
-		'LAST_NAME',
-		'PHONE_NUMBER',
-		'MOBILE_NUMBER',
-		'EMAIL_ID',
-		'DOB',
-	),
-)); ?>
+
+
+<?php
+
+if ($modelFamily != null) {
+    echo "<h4>Family Information</h4>";
+    $this->widget('zii.widgets.CDetailView', array(
+        'data' => $modelFamily,
+        'attributes' => array(
+            'FAMILY_NAME',
+            'DOR',
+            'FAMILY_EMAIL_ID',
+            'FAMILY_PHONE_NUMBER',
+            'FAMILY_DESC',
+            'CREATED_BY',
+        ),
+    ));
+}
+?>
