add158a5f07cbba6c488d95a22cc7256d472c955
prateek <>
2013-06-12 13:28:34 +0530

    added new menu items

--- protected/views/layouts/postPage.php
+++ protected/views/layouts/postPage.php
@@ -16,16 +16,16 @@ and open the template in the editor.
     <body style="background-color:white ">
         <div class="headermain">
             <h1  class="pull-left"> 
-            <?php
-            $this->widget('bootstrap.widgets.TbButton', array(
-                'buttonType' => 'link',
-                'type' => 'primary',
-                'label' => 'Home',
-                'url' => array('site/home'),
-                'htmlOptions' => array('class' => 'pull-right btn ')
-            ));
-            ?>
-                </h1> 
+                <?php
+                $this->widget('bootstrap.widgets.TbButton', array(
+                    'buttonType' => 'link',
+                    'type' => 'primary',
+                    'label' => 'Home',
+                    'url' => array('site/home'),
+                    'htmlOptions' => array('class' => 'pull-right btn ')
+                ));
+                ?>
+            </h1> 
             <?php
             $this->widget('bootstrap.widgets.TbButton', array(
                 'buttonType' => 'link',
@@ -49,7 +49,8 @@ and open the template in the editor.
                             array('label' => 'Family Registration', 'icon' => 'user', 'url' => array('familyRegistration/create'), 'visible' => !Yii::app()->user->hasState("familyId")),
                             array('label' => 'Edit Profile', 'icon' => 'user', 'url' => array('userLoginModel/update')),
                             array('label' => 'View Profile', 'icon' => 'user', 'url' => array('userLoginModel/view')),
-                            array('label' => 'Hobbies', 'icon' => 'home', 'url' =>array('fmUserHobbies/create')),
+                            array('label' => 'Hobbies', 'icon' => 'home', 'url' => array('fmUserHobbies/create')),
+                            array('label' => 'Address', 'icon' => 'cog', 'url' => array('FmAddressMst/create')),
                             array('label' => 'Upload Photo', 'icon' => 'cog', 'url' => array('FmUserPhotos/create')),
                             array('label' => 'Family Tree', 'icon' => 'home', 'url' => '#'),
                             array('label' => 'Help', 'icon' => 'flag', 'url' => '#'),
